f6ac0bfb626ccdd64286e2cf468d1f4ccb25ec3f
Warner Nielsen <bwn888@gmail.com>
2017-06-08 23:04:15 -0600

    Update spacing on webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -22,13 +22,13 @@ module.exports = {
   },
   module: {
     loaders: [
-	    {
-	      loader: 'babel-loader',
-	      query: {
-	        presets: ['react', 'es2015', 'stage-0']
-	      },
-	      test: /\.jsx?$/,
-	      exclude: /(node_modules|bower_components)/
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
       }
     ]
   },
